b30723e44203a2ba013e08345638ab12f7d2871d
Max Gusakov <max@gusakov.com>
2024-08-22 14:02:03 +0300

    Create admin.config.js

---
+++ src/admin/admin.config.js
@@ -0,0 +1,19 @@
+export default {
+  config: {
+    defaultTheme: 'light',
+    theme: {
+      light: {
+        colors: {
+          primary100: '#D1E4FC',
+          primary200: '#AECFFA',
+          primary500: '#8BBBF9',
+          primary600: '#5299F5',
+          primary700: '#1877F2',
+        },
+      },
+    },
+    tutorials: false,
+    notifications: { release: false },
+  },
+  bootstrap() {},
+};
